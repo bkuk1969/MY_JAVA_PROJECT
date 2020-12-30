b239e781a64fcea5f2c26fac2cecb88c4e24d4c4
bkuk69 <richku@gmail.com>
2020-12-30 20:04:30 +0900

    Chapter03

---
+++ src/icehs/science/chapter03/StudentScore.java
@@ -0,0 +1,21 @@
+package icehs.science.chapter03;
+
+public class StudentScore {
+	public static void main(String[] args) {
+		double korean = 87.1;
+		double math = 94.7;
+		double english = 83.2;
+		
+		int intKorean = (int)korean;
+		int intMath = (int)math;
+		int intEnglish = (int)english;
+		
+		System.out.println("수학 : " + (int)math);
+		System.out.println("영어 : " + (int)english);
+		System.out.println("국어 : " + intKorean);
+		
+		
+		
+	}
+
+}
