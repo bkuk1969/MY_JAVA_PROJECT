85f730d2b4390e19b6dd01ed0987193df0d76fbb
bkuk69 <richku@gmail.com>
2021-01-02 15:51:02 +0900

    Chapter05

---
+++ src/icehs/science/chapter05/SumTest.java
@@ -0,0 +1,14 @@
+package icehs.science.chapter05;
+
+public class SumTest {
+	public static void main(String[] args) {
+		int sum = 0;//결과값에 0
+		int i ;
+		for ( i = 0; i <= 100; i++ ) {
+			sum += i; //sum = sum + i;
+			//System.out.println(sum);
+		}
+		System.out.println(sum);
+		System.out.println(i);
+	}
+}
---
+++ src/icehs/science/chapter05/WhyFor.java
@@ -0,0 +1,9 @@
+package icehs.science.chapter05;
+
+public class WhyFor {
+	public static void main(String[] args) {
+		for(int i = 0 ; i < 100; i++) {
+			System.out.println(9);
+		}
+	}
+}
