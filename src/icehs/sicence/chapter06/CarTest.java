3da9c38b25eec1c9cb813c05912fcec938086c24
bkuk69 <richku@gmail.com>
2021-01-04 21:06:37 +0900

    Chapter06

---
+++ src/icehs/sicence/chapter06/CarTest.java
@@ -0,0 +1,23 @@
+package icehs.sicence.chapter06;
+
+public class CarTest {
+	public static void main(String[] args) {
+		Car iceCar = new Car();
+		
+		iceCar.distance = 140;
+		iceCar.oilAmount = 5;
+		
+		iceCar.printCarInfo();
+		
+		iceCar.addOil(10);		
+		iceCar.printCarInfo();
+		
+		iceCar.driveCar("IT센터", "인천공항", 10.17);
+		iceCar.printCarInfo();
+		
+		iceCar.driveCar("인천공항", "곤지암리조트", 60.21);
+		iceCar.printCarInfo();
+		
+		iceCar.calcEnableDistance();
+	}
+}
---
+++ src/icehs/sicence/chapter06/Coffee.java
@@ -0,0 +1,15 @@
+package icehs.sicence.chapter06;
+
+public class Coffee {
+	String name;
+	int price;
+	
+	void printCoffeeInfo() {
+		System.out.println(name + " : " + price +"¿ø");
+	}
+	
+	int getPrice() {
+		return price;
+	}
+	
+}
