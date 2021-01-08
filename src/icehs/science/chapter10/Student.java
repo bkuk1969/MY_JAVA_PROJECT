955d0a685f398c8410c66bbf67da186a9f052053
bkuk69 <richku@gmail.com>
2021-01-08 21:14:41 +0900

    Chapter10

---
+++ src/icehs/science/chapter10/Student.java
@@ -0,0 +1,16 @@
+package icehs.science.chapter10;
+
+public class Student extends Person {
+	
+	String department;
+	
+	@Override
+	void eat() {
+		
+		System.out.println("조별로 먹어요.");
+	}
+	
+	void study() {
+		System.out.println("공부를 합니다.");
+	}
+}
